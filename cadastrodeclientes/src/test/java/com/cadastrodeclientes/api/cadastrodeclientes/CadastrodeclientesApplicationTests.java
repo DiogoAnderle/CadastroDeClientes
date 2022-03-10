8003973bfc34764c6c5b723fbcd3abc5f6ef992d
DiogoAnderle <anderle88@gmail.com>
2022-03-09 21:02:08 -0300

    First Commit

---
+++ cadastrodeclientes/src/test/java/com/cadastrodeclientes/api/cadastrodeclientes/CadastrodeclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastrodeclientes.api.cadastrodeclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
